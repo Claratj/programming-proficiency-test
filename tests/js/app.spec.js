1a4dbd570d3217e43a236077e5ccd0c1a2e3c5eb
ClaraTJ <clara@claratoloba.com>
2023-03-10 17:34:47 +0100

    feat: wip tests

--- tests/js/app.spec.js
+++ tests/js/app.spec.js
@@ -6,4 +6,31 @@ describe('App.vue', () => {
     const wrapper = shallowMount(App);
     expect(wrapper.isVueInstance()).toBe(true);
   });
+
+  // it('should render a filter section', () => {
+    //select
+    //input-search
+    //input-date desde & hasta
+    //create button
+  // });
+
+  // it('should render a table', ()=> {
+    //if !this.filters --> loading
+    //headings of the table
+    //one user
+    //buttons edit && erase
+  // });
+
+  // it('should eliminate the row selected when the erase button is clicked', ()=> {
+  // });
 });
+
+// describe('Modal.vue', () => {
+//   it('should open a modal when Crear is clicked', ()=>{
+//   });
+//   it('should open a modal when Editar is clicked', ()=> {
+
+//   });
+//   it('should render a Modal', ()=> {
+//   });
+// });
